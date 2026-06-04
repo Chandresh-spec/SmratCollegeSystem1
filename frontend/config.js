f0a5c2b3a4f0d599aeb063cb89ae57441e3a7d69
Chandresh-spec <chandumoger819@gmail.com>
2026-06-04 09:30:23 +0530

    Fix CORS issue by using relative API_BASE

--- frontend/config.js
+++ frontend/config.js
@@ -3,4 +3,4 @@
 // const API_BASE = 'http://127.0.0.1:8000';
 
 // Production URL for Render/Railway
-const API_BASE = 'https://smratcollegesystem-production-218f.up.railway.app';
+const API_BASE = '';
